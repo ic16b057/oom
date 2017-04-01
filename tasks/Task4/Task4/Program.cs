1cb2239642ef28acebf113163cd39d6a28e3dc30
Martin Grabner <martin@MartinsMBP7274.macgenius.me>
2017-04-01 21:49:10 +0200

    Added Task4

---
+++ tasks/Task4/Task4/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Task4
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			double LitersAlc = 0;
+			double LitersNAlc = 0;
+			var drinksImported = new IDrink[4];
+			var drinks = new IDrink[4];
+			drinks[0] = new Bier(Bier.Brands.Gösser, Bier.Sizes.Mass);
+			drinks[1] = new SoftDrink("Cola", 0.5);
+			drinks[2] = new Bier(Bier.Brands.Puntigamer, Bier.Sizes.Krügerl);
+			drinks[3] = new SoftDrink("Apfelsaft", 0.3);
+			string json;
+
+			using (StreamWriter file = File.CreateText(@"data.json"))
+			{
+				JsonSerializer serializer = new JsonSerializer();
+				serializer.Serialize(file, drinks);
+			}
+
+			using (StreamReader file = File.OpenText(@"data.json"))
+			{
+				JsonSerializer serializer = new JsonSerializer();
+				drinksImported = (IDrink[])serializer.Deserialize(file, typeof(SoftDrink[]));
+			}
+
+			Console.WriteLine("JSON:");
+			using (StreamReader file = File.OpenText(@"data.json"))
+			{
+				Console.WriteLine(file.ReadToEnd());
+			}
+
+			Console.WriteLine();
+			Console.WriteLine("Ordered drinks:");
+			foreach (IDrink drink in drinksImported)
+			{
+				Console.WriteLine(drink.Name + " " + drink.Size + "l");
+				if (drink.IsAlcoholic)
+					LitersAlc += drink.Size;
+				else
+					LitersNAlc += drink.Size;
+			}
+			Console.WriteLine("\nLiters of alcoholic drinks consumed: " + LitersAlc);
+			Console.WriteLine("Liters of non-alcoholic drinks consumed: " + LitersNAlc);
+		}
+	}
+}
---
+++ tasks/Task4/Task4/Tests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Task4
+{
+	[TestFixture]
+	public class Tests
+	{
+		[Test]
+		public void MyTests()
+		{
+			var test1 = new Bier(Bier.Brands.Gösser, Bier.Sizes.Mass);
+			Assert.AreEqual(test1.Size, 1.0);
+			Assert.AreEqual(test1.IsAlcoholic, true);
+			Assert.AreEqual(test1.Name, "Gösser");
+			var test2 = new SoftDrink("Cola", 1);
+			Assert.AreEqual(test2.Name, "Cola");
+			Assert.AreEqual(test2.Size, 1.0);
+			Assert.AreEqual(test2.IsAlcoholic, false);
+			var test3 = new Bier(Bier.Brands.Ottakringer, Bier.Sizes.Krügerl);
+			Assert.AreEqual(test3.Size, 0.5);
+			var test4 = new Bier(Bier.Brands.Ottakringer, Bier.Sizes.Seiterl);
+			Assert.AreEqual(test4.Size, 0.3);
+		}
+	}
+}
